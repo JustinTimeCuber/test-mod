303b03589eabcad0a6cc81afc6ac2d83cc9732fe
Justin Barker <justintimecuber@gmail.com>
2022-04-12 12:23:40 -0400

    pagging a lot rn

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,6 +1,16 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.OreBlock;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,14 +18,16 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("test_mod");
+
+	public static final Block EXAMPLE_BLOCK = new OreBlock(FabricBlockSettings.copyOf(Blocks.DIRT).luminance(15), UniformIntProvider.create(30, 70));
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.BLOCK, new Identifier("test_mod", "example_block"), EXAMPLE_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier("test_mod", "example_block"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
 	}
 }
---
+++ src/main/java/net/fabricmc/example/GradientBerriesItem.java
@@ -0,0 +1,41 @@
+package net.fabricmc.example;
+
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.Settings;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+public class GradientBerriesItem extends Item {
+    private static final int MAX_USE_TIME = 40;
+
+    public GradientBerriesItem(Settings settings) {
+        super(settings);
+    }
+
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        ItemStack toReturn = super.finishUsing(stack, world, user);
+        if (user instanceof ServerPlayerEntity) {
+            ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)user;
+            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+        }
+
+        if (!world.isClient) {
+            user.removeStatusEffect(StatusEffects.POISON);
+        }
+        return toReturn;
+    }
+}
